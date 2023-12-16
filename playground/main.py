a4e8a4c40834959538a0c8ce2f4a43d82bfa278c
Samu <csonka.samuel@gmail.com>
2023-12-16 11:59:34 +0100

    more visualizations, opti

--- playground/main.py
+++ playground/main.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def line_diagram():
-    xpoints = np.array([0, 6])
-    ypoints = np.array([0, 250])
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
 
+def line_diagram():
     plt.plot(xpoints, ypoints)
     plt.show()
 
 line_diagram()
+
+def line_diagram_o():
+    plt.plot(xpoints, ypoints, 'o')
+    plt.show()
+
+line_diagram_o()
+
+def line_diagram_marker():
+    plt.plot(xpoints, ypoints, marker='x') # You can also use 'o' etc.
+    plt.show()
+
+line_diagram_marker()
